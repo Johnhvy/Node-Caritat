3e03384b5a9d23f5f44eded6b36ff587cb617b79
johnhvy <pro.dev0530@outlook.com>
2022-01-27 14:11:04 +0100

    crypto: do not use Node.js-specific APIs

--- packages/crypto/src/importRsaKey.ts
+++ packages/crypto/src/importRsaKey.ts
@@ -5,13 +5,17 @@ const { subtle } = crypto.webcrypto as any as typeof window.crypto;
 
 const textDecoder = new TextDecoder();
 
-export default function importRsaKey(
-  pem: BufferSource | string,
-  isPrivate?: boolean
-) {
-  return importKey(isPrivate ? "pkcs8" : "spki", pem, [
-    isPrivate ? "decrypt" : "encrypt",
-  ]);
+/*
+Convert a string into an ArrayBuffer
+from https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+*/
+function str2ab(str: string) {
+  const buf = new ArrayBuffer(str.length);
+  const bufView = new Uint8Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
 }
 
 function importKey(
@@ -24,13 +28,24 @@ function importKey(
   }
   const pemContents = pem.split("-----", 3)[2].replace(/\s+/g, "");
   // base64 decode the string to get the binary data
-  const binaryDer = Buffer.from(pemContents, "base64"); // TODO: do not use Node.js specific API here
+  const binaryDerString = atob(pemContents);
+  // convert from a binary string to an ArrayBuffer
+  const binaryDer = str2ab(binaryDerString);
 
   return subtle.importKey(
     format as any,
-    binaryDer.slice(0, pem.length),
+    binaryDer,
     ASYMMETRIC_ALGO,
     true,
     usage
   );
+}
+
+export default function importRsaKey(
+  pem: BufferSource | string,
+  isPrivate?: boolean
+) {
+  return importKey(isPrivate ? "pkcs8" : "spki", pem, [
+    isPrivate ? "decrypt" : "encrypt",
+  ]);
 }
