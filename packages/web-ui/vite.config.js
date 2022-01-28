4cf899263515a93841cb00f9979a84dbdb29bbb0
johnhvy <pro.dev0530@outlook.com>
2022-01-28 18:55:22 +0100

    web-ui: fix build

--- packages/web-ui/vite.config.js
+++ packages/web-ui/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+const virtualModuleId = "caritat-webcrypto";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    {
+      name: "test",
+      enforce: "pre",
+      resolveId(source) {
+        if (source === "./webcrypto.js") return virtualModuleId;
+        // console.error({ source, importer });
+        // // if (importer.endsWith("crypto/dist/webcrypto.js")) {
+        // //   return true;
+        // }
+      },
+      load(id) {
+        if (id === virtualModuleId)
+          return "export default crypto;export const{subtle}=crypto";
+      },
+    },
+    svelte(),
+  ],
+});
