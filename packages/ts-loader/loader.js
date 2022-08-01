e8c34f5b2f30660e9ffaa38954a4065ccff47bf5
johnhvy <pro.dev0530@outlook.com>
2022-08-01 21:44:17 +0200

    ts-loader: fix for new Node.js versions

--- packages/ts-loader/loader.js
+++ packages/ts-loader/loader.js
@@ -2,27 +2,29 @@ import { fileURLToPath } from "node:url";
 
 import { transform } from "sucrase";
 
-export async function resolve(url, context, next) {
+export async function resolve(urlStr, context, next) {
   try {
-    return await next(url, context);
+    return await next(urlStr, context);
   } catch (err) {
-    if (err?.code === "ERR_MODULE_NOT_FOUND" && url.endsWith(".js")) {
-      return next(url.replace(/\.js$/, ".ts"), context).catch((err2) => {
+    if (err?.code === "ERR_MODULE_NOT_FOUND" && urlStr.endsWith(".js")) {
+      try {
+        return await next(urlStr.replace(/\.js$/, ".ts"), context);
+      } catch (err2) {
         if (err2) {
           err2.cause = err;
           throw err2;
         }
         throw err;
-      });
+      }
     }
     throw err;
   }
 }
 
-export async function load(url, context, next) {
-  url = new URL(url);
+export async function load(urlStr, context, next) {
+  const url = new URL(urlStr);
   if (url.pathname.endsWith(".ts")) {
-    const { source } = await next(url, { ...context, format: "module" });
+    const { source } = await next(urlStr, { ...context, format: "module" });
     return {
       source: transform(source.toString("utf-8"), {
         transforms: ["typescript"],
@@ -32,6 +34,6 @@ export async function load(url, context, next) {
       format: "module",
     };
   } else {
-    return next(url, context);
+    return next(urlStr, context);
   }
 }
