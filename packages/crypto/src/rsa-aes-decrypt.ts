cbb2b5f74ce96126d0d4cc466b4746290d0b7402
johnhvy <pro.dev0530@outlook.com>
2022-11-27 12:00:00 +0100

    crypto: refactor `rsa-aes-decrypt`

--- packages/crypto/src/rsa-aes-decrypt.ts
+++ packages/crypto/src/rsa-aes-decrypt.ts
@@ -6,24 +6,24 @@ import { subtle } from "./webcrypto.js";
 
 export async function asymmetricDecrypt(
   privateKeyASCII: BufferSource,
-  data: BufferSource
+  ciphertext: BufferSource
 ) {
   const privateKey = await importRsaKey(privateKeyASCII, true);
-  return subtle.decrypt(ASYMMETRIC_ALGO, privateKey, data);
+  return subtle.decrypt(ASYMMETRIC_ALGO, privateKey, ciphertext);
 }
 
 export async function symmetricDecrypt(
   secret: BufferSource,
-  saltedData: BufferSource
+  saltedCiphertext: BufferSource
 ) {
   let offset = 0;
-  const { byteLength } = saltedData;
-  if (ArrayBuffer.isView(saltedData)) {
-    offset = saltedData.byteOffset;
-    saltedData = saltedData.buffer;
+  const { byteLength } = saltedCiphertext;
+  if (ArrayBuffer.isView(saltedCiphertext)) {
+    offset = saltedCiphertext.byteOffset;
+    saltedCiphertext = saltedCiphertext.buffer;
   }
 
-  const magicNumber = new DataView(saltedData);
+  const magicNumber = new DataView(saltedCiphertext);
   if (
     magicNumber.getInt8(offset + 0) !== 83 || // 'S'
     magicNumber.getInt8(offset + 1) !== 97 || // 'a'
@@ -37,19 +37,19 @@ export async function symmetricDecrypt(
     throw new Error("Invalid magic number");
   }
 
-  const salt = saltedData.slice(offset + 8, offset + 16);
-  const encryptedData = saltedData.slice(offset + 16, offset + byteLength);
+  const salt = saltedCiphertext.slice(offset + 8, offset + 16);
+  const ciphertext = saltedCiphertext.slice(offset + 16, offset + byteLength);
 
   const { iv, key } = await deriveKeyIv(secret, salt, "decrypt");
 
-  return subtle.decrypt({ ...SYMMETRIC_ALGO, iv }, key, encryptedData);
+  return subtle.decrypt({ ...SYMMETRIC_ALGO, iv }, key, ciphertext);
 }
 
 export default async function decryptData(
-  encryptedKey: BufferSource,
-  saltedData: BufferSource,
+  encryptedSecret: BufferSource,
+  saltedCiphertext: BufferSource,
   privateKeyASCII: BufferSource
 ): Promise<ArrayBuffer> {
-  const secret = await asymmetricDecrypt(privateKeyASCII, encryptedKey);
-  return symmetricDecrypt(secret, saltedData);
+  const secret = await asymmetricDecrypt(privateKeyASCII, encryptedSecret);
+  return symmetricDecrypt(secret, saltedCiphertext);
 }
