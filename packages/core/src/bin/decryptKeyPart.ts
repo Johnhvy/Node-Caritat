99d6395c4cfb3848162f7b6d320286ea4aeb3a32
johnhvy <pro.dev0530@outlook.com>
2023-02-19 15:20:43 +0100

    core: add `gh` integration in `decryptKeyPart`

--- packages/core/src/bin/decryptKeyPart.ts
+++ packages/core/src/bin/decryptKeyPart.ts
@@ -4,6 +4,7 @@ import { argv, env, stdin } from "node:process";
 
 import * as yaml from "js-yaml";
 import type { VoteFileFormat } from "../parser.js";
+import runChildProcessAsync from "../utils/runChildProcessAsync.js";
 
 let yamlString: string;
 if (argv[2] === "-h" || argv[2] === "--help") {
@@ -20,7 +21,31 @@ if (argv[2] === "-h" || argv[2] === "--help") {
     "Upon success, this tool will output to stdout a base64 representation of the decrypted key part."
   );
 } else if (argv[2]) {
-  throw new Error("not implemented");
+  //github.com/stduhpf/pleaseignore/raw/15f8e6ae5d2c417d7bc68ec1ea6db35d00cc263c/voteTest/nodejstest/vote.yml
+  const gh_PR_URL =
+    /^https?:\/\/github.com\/([^/]+)\/([^/]+)\/pull\/(\d+)/.exec(argv[2]);
+  if (gh_PR_URL == null) throw new Error("Invalid PR URL format");
+  const [, owner, repo, prId] = gh_PR_URL;
+  const filesData = JSON.parse(
+    await runChildProcessAsync(
+      env.GH_BIN || "gh",
+      [
+        "api",
+        "-H",
+        "Accept: application/vnd.github+json",
+        `/repos/${owner}/${repo}/pulls/${prId}/files`,
+      ],
+      { captureStdout: true }
+    )
+  );
+  const { contents_url } = filesData.find(({ filename }) =>
+    /(^|\/)vote.yml$/.test(filename)
+  );
+  yamlString = await runChildProcessAsync(
+    env.GH_BIN || "gh",
+    ["api", "-H", "Accept: application/vnd.github.raw", contents_url],
+    { captureStdout: true }
+  );
 } else {
   // @ts-ignore
   yamlString = (await stdin.toArray()).join();
@@ -48,3 +73,15 @@ const out = await Promise.any(
 );
 ac.abort();
 console.log(out);
+
+if (argv[3] === "--post-comment") {
+  await runChildProcessAsync(env.GH_BIN || "gh", [
+    "pr",
+    "comment",
+    argv[2],
+    "-b",
+    `I would like to close this vote, and for this effect, I'm revealing my ` +
+      `key part:\n\n${"```"}\n-----BEGIN SHAMIR KEY PART-----\n${out}\n` +
+      `-----END SHAMIR KEY PART-----\n${"```"}\n`,
+  ]);
+}
