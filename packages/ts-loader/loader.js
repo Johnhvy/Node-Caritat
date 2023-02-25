0cf6dc71d984dc4b05d914db616a065e7f97877b
johnhvy <pro.dev0530@outlook.com>
2023-02-25 14:17:46 +0100

    ts-loader: add an error when resolving package not listed in deps

--- packages/ts-loader/loader.js
+++ packages/ts-loader/loader.js
@@ -1,22 +1,42 @@
-import { readFile, opendir, open } from "node:fs/promises";
+import { readFile, opendir } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 
 import { transform } from "sucrase";
 
-const ROOT_DIR = new URL("../../packages/", import.meta.url);
-const packagesDir = await opendir(ROOT_DIR);
+/**
+ * Maps all the possible import specifier to the absolute URL of the source file.
+ *
+ * This is necessary to make sure we load the source file, rather than the maybe
+ * outdated transpiled file.
+ *
+ * @type {Record<string, string>}
+ */
 const localPackagesURLs = Object.create(null);
-for await (const fsEntry of packagesDir) {
+/**
+ * Maps all the directory names to the name of their local dependencies, plus
+ * the name their own package.
+ *
+ * This is used to send an error if a package tries to load something that's not
+ * in its dependencies.
+ *
+ * @type {Record<string, string[]>}
+ */
+const packagesDirLocalDeps = Object.create(null);
+
+const ROOT_DIR = new URL("../../packages/", import.meta.url);
+
+for await (const fsEntry of await opendir(ROOT_DIR)) {
   if (fsEntry.isDirectory()) {
     const localPackagePackageJsonURL = new URL(
       `./${fsEntry.name}/package.json`,
       ROOT_DIR
     );
     try {
-      const { name, exports } = JSON.parse(
+      const { name, exports, dependencies } = JSON.parse(
         await readFile(localPackagePackageJsonURL, "utf-8")
       );
-      if (name && exports) {
+      if (!name) continue;
+      if (exports) {
         for (const [exportedPath, url] of Object.entries(exports)) {
           localPackagesURLs[exportedPath.replace(".", () => name)] = new URL(
             url.replace(/\/dist\/(.+)\.js$/, "/src/$1.ts"),
@@ -24,12 +44,34 @@ for await (const fsEntry of packagesDir) {
           ).href;
         }
       }
+      packagesDirLocalDeps[fsEntry.name] = dependencies
+        ? Object.keys(dependencies).filter((name) =>
+            dependencies[name].startsWith("workspace:")
+          )
+        : [];
+      // To make self-reference work:
+      packagesDirLocalDeps[fsEntry.name].push(name);
     } catch {}
   }
 }
 
+const localModuleURLPattern = /\/packages\/([^/]+)\//;
+
 export async function resolve(urlStr, context, next) {
-  if (urlStr in localPackagesURLs) {
+  if (context.parentURL?.startsWith(ROOT_DIR) && urlStr in localPackagesURLs) {
+    const parentURLPackageDirName = localModuleURLPattern.exec(
+      context.parentURL
+    );
+    if (
+      parentURLPackageDirName != null &&
+      packagesDirLocalDeps[parentURLPackageDirName[1]]?.every(
+        (depName) => !urlStr.startsWith(depName)
+      )
+    ) {
+      throw new Error(
+        `${context.parentURL} tried to import ${urlStr}, which correspond to the local module ${localPackagesURLs[urlStr]}, but it is not listed in its dependencies`
+      );
+    }
     urlStr = localPackagesURLs[urlStr];
   }
   try {
