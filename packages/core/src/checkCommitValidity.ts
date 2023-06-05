76834edc1ac14285c3af7efce93cbd70e1bece3c
Stéphane du Hamel <stephduh@live.fr>
2023-06-05 18:37:58 +0200

    core: add script to check commit validity

---
+++ packages/core/src/checkCommitValidity.ts
@@ -0,0 +1,53 @@
+import path from "path";
+
+import streamChildProcessStdout from "./utils/streamChildProcessStdout.js";
+
+import Vote, { VoteCommit } from "./vote.js";
+
+interface checkCommitArgs {
+  GIT_BIN: string;
+  cwd: string;
+  branch: string;
+  subPath: string;
+  commitSha: string;
+}
+
+export default async function reasonToDiscardCommit({
+  GIT_BIN = "git",
+  cwd,
+  subPath,
+
+  commitSha,
+}: checkCommitArgs) {
+  const spawnArgs = { cwd };
+
+  const vote = new Vote();
+  vote.loadFromFile(path.join(cwd, subPath, "vote.yml"));
+  const gitShow = streamChildProcessStdout(
+    GIT_BIN,
+    [
+      "--no-pager",
+      "show",
+      `${commitSha}`,
+      "-1",
+      "--format=///%H %G? %aN <%aE>",
+      "--name-only",
+    ],
+    spawnArgs
+  );
+  let currentCommit: VoteCommit;
+  for await (const line of gitShow) {
+    if (line.startsWith("///")) {
+      currentCommit = {
+        sha: line.substr(3, 40),
+        signatureStatus: line.charAt(44),
+        author: line.slice(46),
+        files: [],
+      };
+    } else if (line !== "") {
+      currentCommit?.files.push(line);
+    }
+  }
+  const reason = vote.reasonToDiscardCommit(currentCommit);
+  return reason;
+}
