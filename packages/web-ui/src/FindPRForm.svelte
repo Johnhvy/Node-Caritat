cff6c41dfcfc4add9efa1d5646c3afe3d5040487
johnhvy <pro.dev0530@outlook.com>
2023-06-16 13:00:08 +0200

    feat(web-ui): update CSS

--- packages/web-ui/src/FindPRForm.svelte
+++ packages/web-ui/src/FindPRForm.svelte
@@ -25,13 +25,26 @@
   form div {
     display: flex;
     gap: 1ch;
+    flex-direction: column;
   }
+
   label {
     display: inherit;
     gap: inherit;
     flex-grow: 2;
+    line-height: 2.5rem;
   }
   input[type="url"] {
     flex-grow: 2;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    height: 2.5rem;
+  }
+
+  @media screen and (min-width: 800px) {
+    form div {
+      flex-direction: row;
+    }
+
   }
 </style>
--- packages/web-ui/src/GitHubCredentials.svelte
+++ packages/web-ui/src/GitHubCredentials.svelte
@@ -16,20 +16,41 @@
 </p>
 
 <form on:submit={onSubmit}>
-  <label
-    >Username:
-    <input name="username" value={username} /></label
-  >
-  <label
-    >Token:
-    <input
-      name="token"
-      value={token}
-      type="password"
-      placeholder="gho_…"
-    /></label
-  >
-  <button type="submit">Save</button>
+  <div>
+    <label
+      >Username:
+      <input name="username" value={username} /></label
+    >
+    <label
+      >Token:
+      <input
+        name="token"
+        value={token}
+        type="password"
+        placeholder="gho_…"
+      /></label
+    >
+    <button type="submit">Save</button>
+  </div>
 </form>
 
 <p>The token should have at least the <code>repo</code> permissions.</p>
+
+<style>
+  form div {
+    display: flex;
+    gap: 2ch;
+    flex-direction: column;
+  }
+  label{
+    display: inherit;
+    gap: 1ch;
+    justify-content: space-between;
+  }
+
+  @media screen and (min-width: 800px) {
+    form div {
+      flex-direction: row;
+    }
+  }
+</style>
